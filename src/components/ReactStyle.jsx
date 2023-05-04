de76572a51c38426d327f87256264dcd404c2be5
Ghos7h <pras6enjit@gmail.com>
2023-05-04 14:17:07 +0530

    more example of react style and inline style

--- src/components/ReactStyle.jsx
+++ src/components/ReactStyle.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
+import { useState } from 'react';
 
 const ReactStyle = () => {
     const style = {
-        internal: {
-            color: 'blue'
-        }
-    }
+     dark: {
+      color: 'white',
+      backgroundColor: 'black'
+     },
+     light: {
+      color: 'black',
+      backgroundColor: 'white'
+     }
+    };
+    const [theme, setTheme] = useState('light');
+    const [text, setText] = useState('red');
+
+
   return (
     <>
-    <div className='external'>External Style</div>
-    <div style={style.internal}>Internal Style</div>
-    <div style={{color: 'green'}}>Inline Style</div>
+     <div className='external'>External Style</div>
+     <br />
+     <br />
+      <button onClick={()=> setTheme((prev=> prev==='light'?prev='dark':prev='light' ))}>Toggle Theme</button>
+      <div style={theme === 'light'?style.light:style.dark }>       
+        <div>Internal Style</div>
+      </div>
+      <br />
+      <br />
+      <button onClick={()=> setText((prev=> prev==='green'?prev='black':prev='green' ))}>Toggle Text</button>
+      <div style={ text==='green'?{color:'green'}: {color:'black'}}>
+        <div>Inline Style</div>
+      </div>  
     </>
   )
 }
