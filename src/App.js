1e775004093563de23d4ea506d4938fe632e83cb
Ghos7h <pras6enjit@gmail.com>
2023-05-04 12:43:51 +0530

    event handler

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React, {useState} from 'react';
+
+function App() {
+  const [displayCount, setDisplayCount] = useState(0)
+  const getInput = (event) => {
+    console.log('value = ', event.target.value);
+    console.log('name = ', event.target.name);
+  };
+  const clickCount = (click)=>{
+      setDisplayCount((prevCount)=>prevCount+click)
+  }
+  return (
+    <div className="App">
+        <input type="text" placeholder='Add something...' name='input' onChange={getInput}/>
+        <button onClick={()=> clickCount(1)}>Add Number</button>
+        <h3>Number of times <i><u>Add Number</u></i> has been clicked = {displayCount}</h3>
+    </div>
+  );
+}
+
+export default App;
